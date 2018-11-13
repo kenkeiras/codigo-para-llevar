159a5e8e4d4aa81c38b014a2a89bac57ba37515a
kenkeiras <kenkeiras@codigoparallevar.com>
2018-11-13 21:59:08 +0100

    Make gotify reader reconnect 10s after a fail.

--- bash/gotify-reader/gotify-reader.bash
+++ bash/gotify-reader/gotify-reader.bash
@@ -8,4 +8,7 @@ cd $(dirname "$0")
 
 source params.bash
 
-exec websocat "wss://${DOMAIN}/stream?token=${TOKEN}"|xargs -d$'\n' -n1 sh notify.sh
+while [ 1 ];do
+	websocat -U "wss://${DOMAIN}/stream?token=${TOKEN}"|xargs -d$'\n' -n1 sh notify.sh
+	sleep 10
+done
